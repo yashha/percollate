7667cb1b7480246f2c1ac688978e38ed71511676
Y K <yasarkkk@protonmail.com>
2020-02-23 10:58:16 +0100

    Improved tests

--- tests/basic-test.js
+++ tests/basic-test.js
@@ -2,16 +2,39 @@ const test = require('ava');
 const fs = require('fs');
 const percollate = require('..');
 
-test.beforeEach(() => {
-	percollate.configure();
-});
+const testPdf = `${__dirname}/test.pdf`;
+const testHtml = `${__dirname}/test.html`;
+const testEpub = `${__dirname}/test.epub`;
+const testMd = `${__dirname}/test.md`;
 
-test('basic pdf generation', async t => {
-	const fileName = `${__dirname}/test.pdf`;
+async function generateTestFiles() {
 	await percollate.pdf(['https://de.wikipedia.org/wiki/JavaScript'], {
-		output: fileName,
+		output: testPdf,
 		sandbox: false
 	});
-	t.true(fs.existsSync(fileName));
+	await percollate.html(['https://de.wikipedia.org/wiki/JavaScript'], {
+		output: testHtml,
+		sandbox: false
+	});
+	await percollate.epub(['https://de.wikipedia.org/wiki/JavaScript'], {
+		output: testEpub,
+		sandbox: false
+	});
+	await percollate.md(['https://de.wikipedia.org/wiki/JavaScript'], {
+		output: testMd,
+		sandbox: false
+	});
+}
+
+test.beforeEach(async () => {
+	percollate.configure();
+	await generateTestFiles();
+});
+
+test('files exists', async t => {
+	t.true(fs.existsSync(testPdf));
+	t.true(fs.existsSync(testHtml));
+	t.true(fs.existsSync(testEpub));
+	t.true(fs.existsSync(testMd));
 	t.pass();
 });
